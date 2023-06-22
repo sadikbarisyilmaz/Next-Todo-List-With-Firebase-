509d7c846e0a2d75f8b171644a638edfde91acdd
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-06-22 20:10:14 +0300

    initial commit

---
+++ app/ListItem.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+
+function Listitem({ deleteTodo, todos, toggleComplete }) {
+  return (
+    <>
+      {todos.map((todo) => {
+        const labelId = `checkbox-list-label-${todo}`;
+
+        return (
+          <ListItem
+            key={todo.id}
+            secondaryAction={
+              <IconButton
+                onClick={() => deleteTodo(todo.id)}
+                edge="end"
+                aria-label="comments"
+              >
+                <DeleteOutlineIcon />
+              </IconButton>
+            }
+            disablePadding
+          >
+            <ListItemButton
+              role={undefined}
+              onClick={() => toggleComplete(todo)}
+              dense
+            >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={todo.completed !== true ? false : true}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ "aria-labelledby": labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={todo.text} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </>
+  );
+}
+
+export default Listitem;
---
+++ app/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAS3FOeRg5SQAWiEMSo8AmLZM8OofU9U-0",
+  authDomain: "todo-list-7f34e.firebaseapp.com",
+  projectId: "todo-list-7f34e",
+  storageBucket: "todo-list-7f34e.appspot.com",
+  messagingSenderId: "54775648214",
+  appId: "1:54775648214:web:a2eabc354ef0e724379e77",
+  measurementId: "G-V6NDJYDBCH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
